3d3f7ee641aa670f36803549c18c5183a8cfb9e5
ddeshmukh96 <deshmukhdhananjay187@gmail.com>
2025-09-04 15:12:05 +0000

    adding checking num exist

--- helloworld.py
+++ helloworld.py
@@ -1,1 +1,8 @@
-print('hello world')
+def checkifNumExist(L,x):
+    for n in L:
+        if(n==x):
+            return True
+    return False
+
+x = int(input('Enter num'))
+print(checkifNumExist([2,23,523,56,777],x))
