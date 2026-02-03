f50694db722494b450b8651ad160b9ac1653228c
ddeshmukh96 <deshmukhdhananjay187@gmail.com>
2026-02-03 17:41:12 +0000

    recursion revision

--- Recursion.py
+++ Recursion.py
@@ -1,3 +1,23 @@
+""" Factorial of number n """
+
+# n=int(input("Enter no. whose factorial needs to be find"))
+def factorial(n):
+    if n==1:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+# print(factorial(n))
+
+""" Countdown from n to 1 """
+
+def countdown(k):
+    if k==0:
+        return
+    print(k)
+    countdown(k-1)
+
+# countdown(8)
 
 calls=0
 
@@ -24,7 +44,8 @@ for i in range(0,n+1):
 
 print(fibonacci(n,cache_list))
 print(calls)
-"""
+
+r"""
                                   f(5)
                            /              \
                         f(4)              f(3)
@@ -35,4 +56,5 @@ print(calls)
                 /  \
               f(1) f(0)
 
+            f(1)=1 and f(0)=0
 """
